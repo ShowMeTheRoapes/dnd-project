9cdd893cb71ebb7ab1fa38373011af85c88d234e
ahoopes16 <kah4627@truman.edu>
2018-01-23 17:28:00 -0600

    File formatting and getting things set up.

---
+++ SkillsData.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+static public class Class1
+{
+    static String[][] skills = new String[][]
+    {
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        },
+        new String[]
+        {
+            "",
+            "",
+            ""
+        }
+    };
+}
