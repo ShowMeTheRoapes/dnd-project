89193ca3f37b23e845e1a4c1010e85429e4d5f3c
Dustin Roan <jango2106@gmail.com>
2018-01-26 19:04:07 -0600

    Add the AbilitiesData object to hold Abilitiy data descriptions

---
+++ AbilitiesData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+static public class AbilitiesData
+{
+    static String[][] abilities = new String[][] //Attribute, Default Value, Description
+    {
+        new String[]
+        {
+            "Strength",
+            "10",
+            "Strength measures bodily power, athletic training, and the extent to which you can exert raw physical force. It can model any attempt to lift, push, pull, or break something, to force your body through a space, or to otherwise apply brute force to a situation."
+        },
+        new String[]
+        {
+            "Dexterity",
+            "10",
+            "Dexterity measures agility, reflexes, and balance. It can model any attempt to move nimbly, quickly, or quietly, or to keep from falling on tricky footing."
+        },
+        new String[]
+        {
+            "Constitution",
+            "10",
+            "Constitution measures health, stamina, and vital force. The endurance this ability represents is largely passive rather than involving a specific effort on the part of a character or monster."
+        },
+        new String[]
+        {
+            "Intelligence",
+            "10",
+            "Intelligence measures mental acuity, accuracy of recall, and the ability to reason. It com es into play when you need to draw on logic, education, memory, or deductive reasoning."
+        },
+        new String[]
+        {
+            "Wisdom",
+            "10",
+            "Wisdom reflects how attuned you are to the world around you and represents perceptiveness and intuition. It  reflect an effort to read body language, understand som eone’s feelings, notice things about the environment, or care for an injured person."
+        },
+        new String[]
+        {
+            "Charisma",
+            "10",
+            "Charisma measures your ability to interact effectively with others. It includes such factors as confidence and eloquence, and it can represent a charming or commanding personality. It might arise when you try to influence or entertain others, when you try to make an im pression or tell a convincing lie, or when you are navigating a tricky social situation."
+        }
+    };
+}
