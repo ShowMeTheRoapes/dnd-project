e11d46125433a96e14c60c1dc2f4edf95cc7f94b
Dustin Roan <jango2106@gmail.com>
2018-02-01 17:42:10 -0600

    Added the Attribute and AttributesList classes to deal with attributes. Also made the data in AttributesData public

---
+++ dnd-project/Core/BusinessModels/Attribute.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Attribute
+{
+    private const int MAX_MOD_LEVEL = 20;
+    private const int MIN_MOD_LEVEL = 2;
+    private const int MAX_MOD = 10;
+    private const int MIN_MOD = -5;
+    private int value;
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Value{ get { return value; } set { this.value = value; Modifier = calculateModifier(); } }
+    public int Modifier { get; set; }
+
+    public Attribute()
+    {
+        Name = "";
+        Description = "";
+        Value = 0;
+        Modifier = 0;
+    }
+
+    public Attribute(string name, string description, int value)
+    {
+        Name = name;
+        Description = description;
+        Value = value;
+    }
+
+    private int calculateModifier()
+    {
+       if(Value > MAX_MOD_LEVEL)
+        {
+            return MAX_MOD;
+        }
+       else if(Value < MIN_MOD_LEVEL)
+        {
+            return MIN_MOD;
+        }
+        else
+        {
+            return (Value - 10) / 2;
+        }
+
+    }
+}
---
+++ dnd-project/Core/BusinessModels/AttributesList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class AttributesList
+{
+    private const int DEF_VALUE = 10;
+
+    public List<Attribute> Attributes { get; set; }
+
+    public AttributesList()
+    {
+        Attributes = new List<Attribute>();
+
+        foreach (string[] attr in AttributesData.attributes)
+        {
+            Attributes.Add(new Attribute(attr[0], attr[1], DEF_VALUE));
+        }
+    }
+}
