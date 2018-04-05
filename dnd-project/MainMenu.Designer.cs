f16c619777922aa99c9328d96dac4d073d858533
Dustin Roan <dar5543@truman.edu>
2018-04-05 15:54:16 -0500

    Used the form builder to make a main menu with a function exit button

--- dnd-project/MainMenu.Designer.cs
+++ dnd-project/MainMenu.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace dnd_project
 {
-    partial class Form1
+    partial class MainMenu
     {
         /// <summary>
         /// Required designer variable.
@@ -22,16 +22,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
-        }
 
         #endregion
     }
