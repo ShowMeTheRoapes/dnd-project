cdc300ff8dfee443b500af76c109236937eb5e0a
Dustin Roan <dar5543@truman.edu>
2018-07-20 16:20:49 -0500

    Added simulated die rolling with the Die and DieRoller classes

---
+++ dnd-project/Core/BusinessModels/Dice/Die.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace dnd_project.Core.BusinessModels.Dice
+{
+    class Die
+    {
+        private int Sides { get; set; }
+        private static Random r = new Random();
+
+        public Die(int sides)
+        {
+            Sides = sides;
+        }
+
+        public int Roll()
+        {
+            return r.Next(1, Sides);
+        }
+    }
+}
